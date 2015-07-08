3e7e607569b7a182ac34a40e91496674d155eb13
Chris Hayes <chris@moderncognition.com>
2015-07-08 10:14:20 -0400

    Rename fake mock class

--- tests/Repositories/EloquentRepositoryTest.php
+++ tests/Repositories/EloquentRepositoryTest.php
@@ -3,14 +3,14 @@
 use Mockery as m;
 use App\Repositories\EloquentRepository;
 
-class ModelRepository extends EloquentRepository {};
+class FakeModelRepository extends EloquentRepository {};
 
 class EloquentRepositoryTest extends PHPUnit_Framework_TestCase {
 
 	public function setUp() {
 		$this->mockModel = m::mock('Illuminate\Database\Eloquent\Model');
 		$this->mockPaginator = m::mock('App\Paginator');
-		$this->modelRepository = new ModelRepository($this->mockModel, $this->mockPaginator);
+		$this->modelRepository = new FakeModelRepository($this->mockModel, $this->mockPaginator);
 	}
 
 	public function tearDown() {
