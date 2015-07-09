68fd0d9f7b32f37a4a8ee82859cf26725cab68cb
Chris Hayes <chris@moderncognition.com>
2015-07-09 11:06:44 -0400

    More realistic controller examples

--- src/IdeaController.php
+++ src/IdeaController.php
@@ -5,13 +5,23 @@ use App\Queries\Params\IdeaApiQueryParams;
 
 class IdeaController {
 
+	protected $ideaTransformer;
+
+	public function __construct(IdeaTransformer $ideaTransformer) {
+		$this->ideaTransformer = $ideaTransformer;
+	}
+
 	/**
 	 * Get a listing of ideas.
 	 *
 	 * @return Collection
 	 */
 	public function index(IdeaFilterQuery $query, IdeaApiQueryParams $queryParams) {
-		return $query->execute($queryParams);
+		$result = $query->execute($queryParams);
+
+		$response = $this->transformCollection($result, $this->ideaTransformer, 'ideas');
+
+		return self::response($response, 200);
 	}
 
 }
--- src/SoapboxSimpleIdeaController.php
+++ src/SoapboxSimpleIdeaController.php
@@ -5,13 +5,23 @@ use App\Queries\Params\SoapboxSimpleIdeaQueryParams;
 
 class SoapboxSimpleIdeaController {
 
+	protected $ideaTransformer;
+
+	public function __construct(IdeaTransformer $ideaTransformer) {
+		$this->ideaTransformer = $ideaTransformer;
+	}
+
 	/**
 	 * Get a listing of ideas.
 	 *
 	 * @return Collection
 	 */
 	public function index(IdeaFilterQuery $query, SoapboxSimpleIdeaQueryParams $queryParams) {
-		return $query->execute($queryParams);
+		$result = $query->execute($queryParams);
+
+		$response = $this->transformCollection($result, $this->ideaTransformer, 'ideas');
+
+		return self::response($response, 200);
 	}
 
 }
